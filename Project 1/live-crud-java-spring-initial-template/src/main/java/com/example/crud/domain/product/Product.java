472aeecf94b7757f1fd688e76178376cee352759
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-28 21:54:55 -0300

    feat: Create entity Product and interface for JPA, mapping getAllProducts

---
+++ Project 1/live-crud-java-spring-initial-template/src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,22 @@
+package com.example.crud.domain.product;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name="product")
+@Entity(name="product")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private Number price_in_cents;
+}
