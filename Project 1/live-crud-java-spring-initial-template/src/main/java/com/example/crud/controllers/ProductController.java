d3e3bbbca9004d1e46571ca69a08c6210852f8d5
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-30 13:25:51 -0300

    feat: create execptions generate

--- Project 1/live-crud-java-spring-initial-template/src/main/java/com/example/crud/controllers/ProductController.java
+++ Project 1/live-crud-java-spring-initial-template/src/main/java/com/example/crud/controllers/ProductController.java
@@ -4,6 +4,7 @@ package com.example.crud.controllers;
 import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProduct;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,7 +44,7 @@ public class ProductController {
             product.setPrice_in_cents(data.price_in_cents());
             return ResponseEntity.ok(product);
         }else{
-            return ResponseEntity.notFound().build();
+            throw new EntityNotFoundException();
         }
     }
 
@@ -57,7 +58,7 @@ public class ProductController {
             product.setActive(false);
             return ResponseEntity.noContent().build();
         }else{
-            return ResponseEntity.notFound().build();
+            throw new EntityNotFoundException();
         }
     }
 
---
+++ Project 1/live-crud-java-spring-initial-template/src/main/java/com/example/crud/infra/ExceptionDTO.java
@@ -0,0 +1,10 @@
+package com.example.crud.infra;
+
+public class ExceptionDTO{
+
+    String message;
+
+    public ExceptionDTO(String message){
+        this.message = message;
+    }
+}
