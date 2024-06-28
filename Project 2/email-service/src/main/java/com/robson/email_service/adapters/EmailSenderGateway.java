e45674716a47beff7b0e9fb3bd830d25b2492687
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-06-28 20:47:25 -0300

    feat: create email use case and gateway service

---
+++ Project 2/email-service/src/main/java/com/robson/email_service/adapters/EmailSenderGateway.java
@@ -0,0 +1,6 @@
+package com.robson.email_service.adapters;
+
+public interface EmailSenderGateway {
+
+    void sendEmail(String to, String subject, String body);
+}
--- Project 2/email-service/src/main/java/com/robson/email_service/application/EmailSenderService.java
+++ Project 2/email-service/src/main/java/com/robson/email_service/application/EmailSenderService.java
@@ -1,4 +1,27 @@
 package com.robson.email_service.application;
 
-public class EmailSenderService {
+import com.robson.email_service.adapters.EmailSenderGateway;
+import com.robson.email_service.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGateway = emailGateway;
+    }
+
+
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+
+    }
 }
