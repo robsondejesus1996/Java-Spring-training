2fb695d099195fa0aafeaf5a949764a1960e05d2
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-09-02 21:46:42 -0300

    feat created interface repository

--- Med Voll/api/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ Med Voll/api/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,11 @@
 package med.voll.api.controller;
 
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoReposiory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoReposiory reposiory;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
-
-
+        reposiory.save(new Medico(dados));
     }
 }
